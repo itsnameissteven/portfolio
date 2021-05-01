ea1b1e731bfd4a27afdd50a6cbbe26c9622672fc
Steven Mancine <stevenrmancine@gmail.com>
2021-05-01 09:19:44 -0600

    Create Nav component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Route,  } from 'react-router-dom';
 import './App.scss';
 import ContactPage from '../ContactPage/ContactPage';
+import Nav from '../Nav/Nav';
 
 const App = () => {
   return (
     <div className="App">
-      <nav>
-
-      </nav>
+      <Nav />
       <Route path='/contact' component={ContactPage} /> 
     </div>
   );
