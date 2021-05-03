8de420f74c9e20a5c13dc87f3429f12f27c9fb61
Steven Mancine <stevenrmancine@gmail.com>
2021-05-02 19:25:51 -0600

    Add dynamic hamburger menu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route,  } from 'react-router-dom';
 import ContactPage from '../ContactPage/ContactPage';
 import Nav from '../Nav/Nav';
 
 const App = () => {
+  const [dropDown, setDropDown] = useState(false);
+
+  const closeDropDown = () => {
+    if(dropDown) {
+      setDropDown(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <Nav />
+    <div className="App" onClick={closeDropDown}>
+      <Nav dropDown={dropDown} openDropDown={() => setDropDown(!dropDown)}/>
       <Route path='/contact' component={ContactPage} /> 
     </div>
   );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { CgMenu } from 'react-icons/cg';
 
 
-const Nav = () => {
+const Nav = ({ dropDown, openDropDown}) => {
+
+
   return(
     <nav className='nav'>
       <h1 className="nav__header">It me Steven</h1>
-      <NavLink 
-        className="nav__link" 
-        to='/about-me' 
-        activeClassName="nav__link--active"
-      >
-        About
-      </NavLink>
-      <NavLink 
-        className="nav__link" 
-        to='/projects' 
-        activeClassName="nav__link--active"
-      >
-        Projects
-      </NavLink>
-      <NavLink 
-        className="nav__link" 
-        to='/resume' 
-        activeClassName="nav__link--active"
-      >
-        Resume
-      </NavLink>
-      <NavLink 
-        className="nav__link" 
-        to='/contact' 
-        activeClassName="nav__link--active"
-      >
-        Contact
-      </NavLink>
+      <CgMenu className="nav__hamburger" onClick={openDropDown}/>
+      <div className={`nav__link__container ${dropDown ? "unhide" : null}`}>
+        <NavLink 
+          className="nav__link" 
+          to='/about-me' 
+          activeClassName="nav__link--active"
+        >
+          About
+        </NavLink>
+        <NavLink 
+          className="nav__link" 
+          to='/projects' 
+          activeClassName="nav__link--active"
+        >
+          Projects
+        </NavLink>
+        <NavLink 
+          className="nav__link" 
+          to='/resume' 
+          activeClassName="nav__link--active"
+        >
+          Resume
+        </NavLink>
+        <NavLink 
+          className="nav__link" 
+          to='/contact' 
+          activeClassName="nav__link--active"
+        >
+          Contact
+        </NavLink>
+      </div>
     </nav>
   )
 }
