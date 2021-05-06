8216993bc11fbffda5f69be5e5c229d679ae765c
Steven Mancine <stevenrmancine@gmail.com>
2021-05-06 17:10:12 -0600

    Add initial project images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import rancidTomatillos from '../../assets/rancid_tomatillos.png';
+import metAPI from '../../assets/metAPI.png';
+import overlook from '../../assets/overlook.png';
+import Brew from '../../assets/brew_by_design.png';
 
 const Projects = () => {
   return (
     <div className="projects">
-      
+      <div className='project'>
+        <img className="project__img" src={rancidTomatillos} alt='Rancid Tomatillos' />
+        <div className="project__about">
+
+        </div>
+      </div>
+      <div className='project'>
+        <img className="project__img" src={metAPI} alt='Rancid Tomatillos' />
+        <div className="project__about">
+
+        </div>
+      </div>
+      <div className='project'>
+        <img className="project__img" src={Brew} alt='Rancid Tomatillos' />
+        <div className="project__about">
+
+        </div>
+      </div>
+      <div className='project'>
+        <img className="project__img" src={overlook} alt='Rancid Tomatillos' />
+        <div className="project__about">
+
+        </div>
+      </div>
     </div>  
   )
 }
